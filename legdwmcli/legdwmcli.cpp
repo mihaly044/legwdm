76f582b7c321e31e7e07c920acc5635e960cbfe0
mihaly044 <mihaly044@gmail.com>
2019-03-31 14:55:54 +0200

    Put tests into separate functions, added read and invalid ctl test

--- legdwmcli/legdwmcli.cpp
+++ legdwmcli/legdwmcli.cpp
@@ -35,29 +35,9 @@ DWORD GetProcessId(LPCWSTR ProcessName)
 	return 0;
 }
 
-int wmain(int argc, wchar_t** argv)
+void test_memory_write(HANDLE hDev, DWORD pid)
 {
-
-	// Get the ID of the target process
-	const auto pid = GetCurrentProcessId(); //GetProcessId(argv[1]);
-
-	// Exit if PID was 0
-	if (pid < 1)
-	{
-		wcout << L"Invalid proceess ID " << pid << " . Exiting.";
-		return 0;
-	}
-
-	// Try to connect to the driver
-	const auto h_dev = CreateFile(LEGDWM_USERMODE_PATH, GENERIC_ALL, 0, nullptr, OPEN_EXISTING, FILE_ATTRIBUTE_SYSTEM,
-		nullptr);
-
-	// Exit if we couldn't connect to the driver
-	if (h_dev == INVALID_HANDLE_VALUE)
-	{
-		std::wcout << L"Could not open driver" << std::endl;
-		return 0;
-	}
+	wcout << __FUNCTIONW__ << "():" << endl;
 
 	// This variable holds the value which we will tamper with from inside our driver
 	DWORD value = 123;
@@ -81,15 +61,89 @@ int wmain(int argc, wchar_t** argv)
 	};
 
 	// Talk to the driver
-	if (DeviceIoControl(h_dev, IOCTL_LGCOPYMEMORY, &request, sizeof(request), nullptr,
+	if (DeviceIoControl(hDev, IOCTL_LGCOPYMEMORY, &request, sizeof(request), nullptr,
 		0, &bytesIo, nullptr))
 	{
-		wcout << "The value is " << value << endl;
+		wcout << "The value is " << value << endl << endl;
 	}
 	else
 	{
-		wcout << "Failed talking to the driver!" << endl;
+		wcout << "Failed talking to the driver!" << endl << endl;
+	}
+}
+
+void test_memory_read(HANDLE hDev, DWORD pid)
+{
+	wcout << __FUNCTIONW__ << "():"  <<endl;
+
+	// This variable holds the value which we will tamper with from inside our driver
+	DWORD value = 123;
+
+	// We will copy the previous variable's value into this
+	DWORD read = 0;
+
+	// This replace represents how many bytes were written to/read from our driver
+	DWORD bytesIo = 0;
+
+	// This is the request we are going to pass to our driver
+	LGCOPYMEMORY_REQ request = {
+		FALSE,						// specifies that we are READING from the memory
+		pid,						// target process ID
+		&value,						// target memory address
+		&read,						// copy bytes begining from this address
+		sizeof(value)				// the count of the bytes we will write
+	};
+
+	// Talk to the driver
+	if (DeviceIoControl(hDev, IOCTL_LGCOPYMEMORY, &request, sizeof(request), nullptr,
+		0, &bytesIo, nullptr))
+	{
+		wcout << "The read value is " << read << endl << endl;
+	}
+	else
+	{
+		wcout << "Failed talking to the driver!" << endl << endl;
+	}
+}
+
+void test_invalid_ctl(HANDLE hDev)
+{
+	DWORD bytesIo;
+
+	// In this simple test we will send an invlaid control code to our driver
+	// and check the results in WinDbg
+	DeviceIoControl(hDev, IOCTL_INVALID, nullptr, 0, nullptr,
+		0, &bytesIo, nullptr);
+}
+
+int wmain(int argc, wchar_t** argv)
+{
+
+	// Get the ID of the target process
+	const auto pid = GetCurrentProcessId(); //GetProcessId(argv[1]);
+
+	// Exit if PID was 0
+	if (pid < 1)
+	{
+		wcout << L"Invalid proceess ID " << pid << " . Exiting.";
+		return 0;
 	}
 
-	CloseHandle(h_dev);
+	// Try to connect to the driver
+	const auto hDev = CreateFile(LEGDWM_USERMODE_PATH, GENERIC_ALL, 0, nullptr, OPEN_EXISTING, FILE_ATTRIBUTE_SYSTEM,
+		nullptr);
+
+	// Exit if we couldn't connect to the driver
+	if (hDev == INVALID_HANDLE_VALUE)
+	{
+		std::wcout << L"Could not open driver" << std::endl;
+		return 0;
+	}
+
+	test_memory_write(hDev, pid);
+	test_memory_read(hDev, pid);
+	test_invalid_ctl(hDev);
+
+	// Close the device handler
+	CloseHandle(hDev);
 }
